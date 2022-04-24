e3acc608248b668a2f23f8f8d1abb58087f3e292
oubinamosteiroruben <oubinamosteiroruben@gmail.com>
2022-04-24 22:43:35 +0200

    Comprobaciones de los parámetros

--- Enso_Implementacion/src/GestorAlarmas/GestorAlarmas.java
+++ Enso_Implementacion/src/GestorAlarmas/GestorAlarmas.java
@@ -1,10 +1,5 @@
 package GestorAlarmas;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-
 import GestorEstadisticas.InterfazGestorEstadisticas;
 
 public class GestorAlarmas implements InterfazGestorAlarmas{
@@ -27,11 +22,19 @@ public class GestorAlarmas implements InterfazGestorAlarmas{
 
 
 	@Override
-	public Boolean anhadirAlarma(Date fechaInicio, String tipo, String centro, String estado) {
+	public static Boolean anhadirAlarma(Date fechaInicio, String tipo, String centro, String estado) {
+		
 		try {
+			
+			/*Comprobación de los parámetros*/
+			
+			if(tipo.equals("") || centro.equals("") || estado.equals("")) { /* en tipo, estado se podrían poner constantes */
+				return false;
+			}
+			
 			InterfazAlarma ia = (InterfazAlarma) new Alarma(contadorAlarmas++,fechaInicio,tipo,centro,estado);
-			this.alarmas.put(ia.getIdAlarma(),ia);
-			this.gestorEstadisticas.anhadirAlarma(ia);
+			alarmas.put(ia.getIdAlarma(),ia);
+			gestorEstadisticas.anhadirAlarma(ia);
 		}catch(Exception e) {
 			return false;
 		}
@@ -41,28 +44,62 @@ public class GestorAlarmas implements InterfazGestorAlarmas{
 
 	@Override
 	public Boolean cerrarAlarma(Integer idAlarma) {
-		return this.alarmas.get(idAlarma).cerrarAlarma();
+		try {
+			return this.alarmas.get(idAlarma).cerrarAlarma();
+		}catch(Exception e) {
+			return false;
+		}	
 	}
 
 
 	@Override
 	public Boolean modificarEstadoAlarma(Integer idAlarma, String estado) {
-		return this.alarmas.get(idAlarma).modificarAlarma(estado);
+		try {
+			if(estado.equals("")) {
+				return false;
+			}
+			return this.alarmas.get(idAlarma).modificarAlarma(estado);
+		}catch(Exception e) {
+			return false;
+		}
+		
 	}
 
 
 	@Override
 	public Boolean anhadirProtocolo(String tipoAlarma, List<String> acciones) {
 		
-		//return 	this.gestorEquipos.anhadirProtocolo(new Protocolo(contadorProtocolos++,tipoAlarma,acciones));
-		return this.gestorEquipos.anhadirProtocolo(tipoAlarma, acciones); // que el contador estÃ© en el gestor de equipos
+		try {
+			if(tipoAlarma.equals("") || acciones.length() == 0) {
+				return false;
+			}
+			
+			for(String a: acciones) { // Las acciones no pueden ser vacías
+				if(a.equals("")) {
+					return false;
+				}
+			}
+			
+			//return 	this.gestorEquipos.anhadirProtocolo(new Protocolo(contadorProtocolos++,tipoAlarma,acciones));
+			return this.gestorEquipos.anhadirProtocolo(tipoAlarma, acciones); // que el contador estÃ© en el gestor de equipos
+			
+		}catch(Exception e) {
+			return false;
+		}
+		
 		
 	}
 
 
 	@Override
 	public Boolean anhadirCentralita(String centro) {
+		
 		try {
+			
+			if(centro.equals("")) {
+				return false;
+			}
+			
 			InterfazCentralita ic = (InterfazCentralita) new Centralita(contadorCentralitas++,centro,this);
 			this.centralitas.put(ic.getIdCentralita(),ic);
 		}catch(Exception e) {
